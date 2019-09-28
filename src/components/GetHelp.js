4b025aa26ea55d368376de03095a84a874538608
Wasique <wasique13@gmail.com>
2019-09-27 22:24:08 -0400

    added google map

--- src/components/GetHelp.js
+++ src/components/GetHelp.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Iframe from 'react-iframe'
 
 const Home = () => (
   <div>
     <h1>Get Help!</h1>
+    <Iframe 
+    url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12093.130728637721!2d-74.05051713150046!3d40.73380495773597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25754031f991f%3A0xecdb9dbbff731347!2sJP%20Morgan!5e0!3m2!1sen!2sus!4v1569634178474!5m2!1sen!2sus" 
+    width="600" 
+    height="450" 
+    frameborder="0" 
+    style="border:0;" 
+    allowfullscreen="">
+    </Iframe>
   </div>
 )
 
