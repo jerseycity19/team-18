01727bb96826f8554d1a97a5612f5a7524888260
vodewey12 <voduy12@gmail.com>
2019-09-28 08:17:59 -0400

    style changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
 import './Header.css';
-import logo from './logo.png';
-
-// The Header creates links that can be used to navigate
-// between routes.
-const Header = () => (
-<div id="parent">
+import { Link } from 'react-router-dom';
+import logo from './logo.png'
 
 
-<div id="sideContent1">
-
-<img width="150px" className="logo" src={logo} alt="logo" />
-
-
-</div>
-
-
-<div id="mainContent1">
-
-  <header>
-
-    <nav>
-      <ul className="ulh">
-        <li className="lih"><Link to='/roster'>Roster</Link></li>
-        <li className="lih"><Link to='/communityForum'>Community Forum</Link></li>
-        <li className="lih"><Link to='/getHelp'>Get Help</Link></li>
-        <li className="lih"><Link to='/'>Home</Link></li>
-
-      </ul>
-    </nav>
-  </header>
-</div>
-
-</div>
-
-)
+const Header= () => (
+    <div class='topnav'>
+      <img class="logo" color="background-white"width="150px" className="logo" src={logo} alt="logo"/>
+      <nav>
+        <ul className="ulh">
+          <li className="lih"><Link to='/communityForum'>Community Forum</Link></li>
+          <li className="lih"><Link to='/getHelp'>Get Help</Link></li>
+          <li className="lih"><Link to='/'>Home</Link></li>
+  
+        </ul>
+      </nav>
+      
+      
+    </div>
+);
 
 export default Header
