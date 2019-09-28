0780ca53592cb237347f17a52dff2b98a3267b33
garyung171 <garyung17@gmail.com>
2019-09-28 06:30:15 -0400

    these warning clog up the console

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,12 @@ import logo from './logo.png';
 // The Header creates links that can be used to navigate
 // between routes.
 const Header = () => (
-<div id = "parent">
+<div id="parent">
 
 
 <div id="sideContent1">
 
-<img width = "150px " className="logo" src={logo} alt="logo" />
+<img width="150px" className="logo" src={logo} alt="logo" />
 
 
 </div>
@@ -22,11 +22,11 @@ const Header = () => (
   <header>
 
     <nav>
-      <ul className = "ulh">
-        <li className = "lih"><Link to='/roster'>Roster</Link></li>
-        <li className = "lih"><Link to='/communityForum'>Community Forum</Link></li>
-        <li className = "lih"><Link to='/getHelp'>Get Help</Link></li>
-        <li className = "lih"><Link to='/'>Home</Link></li>
+      <ul className="ulh">
+        <li className="lih"><Link to='/roster'>Roster</Link></li>
+        <li className="lih"><Link to='/communityForum'>Community Forum</Link></li>
+        <li className="lih"><Link to='/getHelp'>Get Help</Link></li>
+        <li className="lih"><Link to='/'>Home</Link></li>
 
       </ul>
     </nav>
